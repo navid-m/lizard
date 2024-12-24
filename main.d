03cb43f9f0f4c50ea99d0b43f8b5df243b4eece2
Navid M <navidmomtahen@gmail.com>
2024-12-24 20:06:46 +0000

    extras

--- main.d
+++ main.d
@@ -2,9 +2,12 @@ module main;
 
 import core.sys.windows.windows;
 import core.sys.windows.psapi;
+import std.datetime;
 import std.stdio;
 import std.string;
 import core.sys.windows.windows;
+import core.thread.osthread;
+import core.time;
 import std.algorithm.searching;
 
 /** 
@@ -220,8 +223,20 @@ void main()
     auto pm = ProcessMemory.fromWindowTitle("Cube 2: Sauerbraten");
     if (pm !is null)
     {
-        string valuer;
-        pm.readChainMemory("sauerbraten.exe", 0x2A5730, [0x274], valuer);
-        writeln("Current player: ", valuer);
+        while (true)
+        {
+            string valuer;
+            pm.readChainMemory("sauerbraten.exe", 0x2A5730, [0x274], valuer);
+            writeln("Current player: ", valuer);
+            byte[10] isAimingAtEnemy;
+            pm.readChainMemory("sauerbraten.exe", 0x26DD08, [0x8C], isAimingAtEnemy);
+            writeln(isAimingAtEnemy);
+            byte[4] isShooting;
+            pm.readChainMemory("sauerbraten.exe", 0x2A5730, [0x1FC], isShooting);
+            writeln(isShooting);
+
+            Thread.sleep(1.seconds);
+        }
+
     }
 }
