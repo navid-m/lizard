92d14cb77bb74707465934ef404d84d43205bc07
Navid M <navidmomtahen@gmail.com>
2024-12-25 00:21:00 +0000

    format

--- source/app.d
+++ source/app.d
@@ -15,14 +15,16 @@ void main()
     auto pm = ProcessMemory.fromWindowTitle("Cube 2: Sauerbraten");
     if (pm !is null)
     {
+        string valuer;
+        pm.readChainMemory("sauerbraten.exe", 0x2A5730, [0x274], valuer);
+        writeln("Current player: ", valuer);
+
         ulong[4] towrite = [1, 101, 110, 100];
         ulong[4] tostop = [0, 101, 110, 100];
 
         while (true)
         {
-            // string valuer;
-            // pm.readChainMemory("sauerbraten.exe", 0x2A5730, [0x274], valuer);
-            // // writeln("Current player: ", valuer);
+
             byte[10] isAimingAtEnemy;
             pm.readChainMemory("sauerbraten.exe", 0x26DD08, [0x83C], isAimingAtEnemy);
             if (isAimingAtEnemy[0] != -54)
@@ -37,6 +39,5 @@ void main()
             pm.readChainMemory("sauerbraten.exe", 0x2A5730, [0x1FC], isShooting);
             Thread.sleep(1.msecs);
         }
-
     }
 }
